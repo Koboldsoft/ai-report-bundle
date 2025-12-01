1989e7ffa6c2e4642210b9b530c55ee100d42ad1
Koboldsoft <info@koboldsoft.com>
2025-12-01 14:06:12 +0100

    new repository mm_auftrag and entity mm_auftrag

---
+++ src/Entity/MmAuftrag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Koboldsoft\AiReportBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\AiReportBundle\Repository\MmAuftragRepository")
+ * @ORM\Table(name="mm_auftrag")
+ */
+class MmAuftrag
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $einheiten_ist;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEinheitenIst(): ?float
+    {
+        return $this->einheiten_ist;
+    }
+
+    public function setEinheitenIst(?float $einheitenIst): self
+    {
+        $this->einheiten_ist = $einheitenIst;
+        return $this;
+    }
+}
