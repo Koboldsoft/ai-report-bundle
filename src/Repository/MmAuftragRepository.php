c38d55f62cde21de6718b9db4b74c74917800197
Koboldsoft <info@koboldsoft.com>
2025-12-01 14:06:12 +0100

    new repository mm_auftrag and entity mm_auftrag

---
+++ src/Repository/MmAuftragRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Koboldsoft\AiReportBundle\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Koboldsoft\AiReportBundle\Entity\MmAuftrag;
+
+class MmAuftragRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MmAuftrag::class);
+    }
+
+    /**
+     * Find an Auftrag by its ID (returns one entity or null)
+     */
+    public function findAuftragById(int $id): ?MmAuftrag
+    {
+        return $this->find($id);
+    }
+}
